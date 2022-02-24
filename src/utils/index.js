5ac0d164b7999ea4f76f688ed29040327603a6e2
daifazhan <daifzh@163.com>
2022-02-24 21:09:37 +0800

    feat: 添加工程列表

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+const isFalsy = (value) => (value === 0 ? false : !value);
+
+export const cleanObject = (object) => {
+  const result = { ...object };
+
+  Object.keys(result).forEach((key) => {
+    if (isFalsy(result[key])) {
+      delete result[key];
+    }
+  });
+  return result;
+};

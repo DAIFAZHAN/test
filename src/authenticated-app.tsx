a2756a646a60d4557ed9c14b1467869cc7abfcdd
daifazhan <daifzh@163.com>
2022-03-10 19:59:49 +0800

    feat: 用grid和flex布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,67 @@
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
+import styled from "@emotion/styled";
+
+/**
+ * grid 和 flex 各自的应用场景
+ * 1. 要考虑，是一维布局 还是 二维布局
+ * 一般来说，一维布局用flex，二维布局用grid
+ * 2. 是从内容出发还是从布局出发？
+ * 从内容出发：你先有一组内容(数量一般不固定),然后希望他们均匀的分布在容器中，由内容自己的大小决定占据的空间
+ * 从布局出发：先规划网格(数量一般比较固定)，然后再把元素往里填充
+ * 从内容出发，用flex
+ * 从布局出发，用grid
+ *
+ */
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
 
+  // return (
+  //   <div>
+  //     <button onClick={() => logout()}>登出</button>
+  //     <ProjectListScreen />
+  //   </div>
+  // );
   return (
-    <div>
-      <button onClick={() => logout()}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <p>logo</p>
+          <p>项目</p>
+          <p>用户</p>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={() => logout()}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+const Header = styled.header`
+  grid-area: header; //这种情况下加grid-area为何不是从第一个开始排？并且分了两列？
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  //grid-area: main;//加上为何会显示在第三行第二列。
+`;

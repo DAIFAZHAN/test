bec936d9ffb7e5a698d6fcbc792b7536a4ca84fb
daifazhan <daifzh@163.com>
2022-03-11 03:27:10 +0800

    feat: 添加loading和Error状态，增加页面友好性

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,6 +6,7 @@ import qs from "qs";
 import { cleanObject, useDebounce, useMount } from "../../utils";
 import { useHttp } from "../../utils/http";
 import styled from "@emotion/styled";
+import { Typography } from "antd";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -16,24 +17,35 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<null | Error>(null);
   const debouncedParam = useDebounce(param, 2000);
 
   const client = useHttp();
 
   useEffect(() => {
-    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+    setIsLoading(true);
+    client("projects", { data: cleanObject(debouncedParam) })
+      .then(setList)
+      .catch((error) => {
+        setList([]);
+        setError(error);
+      })
+      .finally(() => setIsLoading(false));
   }, [debouncedParam]);
 
   useMount(() => {
     client("users").then(setUsers);
   });
 
-  console.log("ProjectListScreen");
   return (
     <Container>
       <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
-      <List users={users} list={list} />
+      {error ? (
+        <Typography.Text type={"danger"}>{error.message}</Typography.Text>
+      ) : null}
+      <List users={users} dataSource={list} loading={isLoading} />
     </Container>
   );
 };

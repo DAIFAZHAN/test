f86ea3ee92afd8fd862ac19b699cff0be2969dd8
daifazhan <daifzh@163.com>
2022-03-14 17:12:48 +0800

    feat: kanban和epic路由

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { Navigate } from "react-router-dom";
+import { KanbanScreen } from "../kanban";
+import { EpicScreen } from "../epic";
 
 export const ProjectScreen = () => {
-  return <h1>ProjectScreen</h1>;
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <Link to={"kanban"}>看板</Link>
+      <Link to={"epic"}>任务组</Link>
+      <Routes>
+        <Route path={"/kanban"} element={<KanbanScreen />} />
+        <Route path={"/epic"} element={<EpicScreen />} />
+        <Route
+          path={"/"}
+          element={<Navigate to={window.location.pathname + "/kanban"} />}
+        />
+      </Routes>
+    </div>
+  );
 };

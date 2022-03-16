e9c3040201bd8dcba713b79f40a1b1e2ea334a98
daifazhan <daifzh@163.com>
2022-03-17 02:13:20 +0800

    feat: 星号功能，点击可以进行服务器端的pin属性的修改，但页面未重新渲染刷新星号

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -3,6 +3,8 @@ import { User } from "./search-panel";
 import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
+import { Pin } from "components/pin";
+import { useEditProject } from "utils/project";
 
 export interface Project {
   name: string;
@@ -18,11 +20,25 @@ interface ListProps extends TableProps<Project> {
 }
 
 export const List = ({ users, ...props }: ListProps) => {
+  const { mutate } = useEditProject();
+  const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin });
+
   return (
     <Table
       rowKey={"id"}
       pagination={false}
       columns={[
+        {
+          title: <Pin checked={true} disabled />,
+          render(value, project) {
+            return (
+              <Pin
+                checked={project.pin}
+                onCheckedChange={pinProject(project.id)}
+              />
+            );
+          },
+        },
         {
           title: "项目",
           sorter: (a, b) => a.name.localeCompare(b.name),
--- src/utils/project.ts
+++ src/utils/project.ts
@@ -15,3 +15,30 @@ export const useProject = (debouncedParam?: Partial<Project>) => {
 
   return result;
 };
+
+export const useEditProject = () => {
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
+  const mutate = (params: Partial<Project>) => {
+    return run(
+      client(`projects/${params.id}`, { data: params, method: "PATCH" })
+    );
+  };
+  return {
+    mutate,
+    ...asyncResult,
+  };
+};
+
+export const useAddProject = () => {
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
+  const mutate = (params: Partial<Project>) => {
+    // TODO
+    return run(client("projects", { data: params, method: "POST" }));
+  };
+  return {
+    mutate,
+    ...asyncResult,
+  };
+};

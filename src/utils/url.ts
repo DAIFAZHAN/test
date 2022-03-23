99c9f88a665a909a9d280320ee8826f53d3362e4
daifazhan <daifzh@163.com>
2022-03-23 17:02:49 +0800

    chore: 改变setSearchParam的实现方式，使用统一接口useSetUrlSearchParam

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,7 +3,8 @@ import { useMemo, useState } from "react";
 import { cleanObject } from "utils";
 
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParam] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const setSearchParam = useSetUrlSearchParam();
   const [stateKeys] = useState(keys);
   return [
     useMemo(
@@ -15,15 +16,12 @@ export const useUrlQueryParam = <K extends string>(keys: K[]) => {
     ),
     // setSearchParam,
     (params: Partial<{ [key in K]: unknown }>) => {
-      const o = cleanObject({
-        ...Object.fromEntries(searchParams),
-        ...params,
-      }) as URLSearchParamsInit;
-      setSearchParam(o);
+      setSearchParam(params);
     },
   ] as const;
 };
 
+//setSearchParam的通用hook
 export const useSetUrlSearchParam = () => {
   const [searchParams, setSearchParam] = useSearchParams();
   return (params: { [key in string]: unknown }) => {
